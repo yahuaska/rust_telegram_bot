72dfce899ce3c6977aa055184ee29138e6a2cf93
Denis Bukanov <denis.bukanov@cloud.ionos.com>
2025-10-29 15:04:55 +0100

    Simple demo for requests

---
+++ src/main.rs
@@ -0,0 +1,85 @@
+use crate::api_client::ApiClient;
+use crate::commands::{decide_command, Command};
+use crate::http_clients::ReqwestHttpClient;
+use crate::types::BotConfig;
+use crate::types::GetMeResponse;
+use std::env;
+
+pub mod api_client;
+pub mod commands;
+pub mod http_clients;
+pub mod types;
+
+fn print_me(resp: GetMeResponse) {
+    if resp.ok {
+        println!("Status: OK");
+        println!(
+            "My ID: {}\nMy name is: {} ({})",
+            resp.result.id, resp.result.first_name, resp.result.username
+        );
+        if resp.result.is_bot {
+            println!("I am a bot!");
+        } else {
+            println!("I am not a bot!");
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    const TOKEN_KEY: &str = "TOKEN";
+    let bot_config = match env::var(TOKEN_KEY) {
+        Ok(val) => BotConfig {
+            token: val.to_owned(),
+            offset: 0,
+            polling_timeout: 30,
+        },
+        Err(_) => {
+            println!("Warning: {} is not set. Using default token.", TOKEN_KEY);
+            return;
+        }
+    };
+    let mut api_client = ApiClient::new(
+        ReqwestHttpClient::new(),
+        |token: &str, method: &str| format!("https://api.telegram.org/bot{token}/{method}"),
+        bot_config,
+    );
+    print_me(api_client.get_me().await.unwrap());
+    loop {
+        let updates = match api_client.get_updates().await {
+            Some(updates) => updates,
+            None => {
+                continue;
+            }
+        };
+        for update in &updates.result {
+            api_client.update_offset(update.update_id);
+            println!(
+                "Update ID: {}\n{}",
+                update.get_update_id(),
+                match (update.edited_message.as_ref(), update.message.as_ref()) {
+                    (Some(edited_message), _) => format!("Edited message: {}", edited_message),
+                    (_, Some(message)) => format!("New message: {}", message),
+                    _ => "No message".to_string(),
+                }
+            );
+            let message = match (&update.message, &update.edited_message) {
+                (Some(msg), None) => msg,
+                (None, Some(edited_msg)) => edited_msg,
+                _ => {
+                    println!("No message");
+                    continue;
+                }
+            };
+            let cmd = decide_command(message);
+            match cmd {
+                Some(Command { command, args }) => {
+                    println!("Command: {:#?}, args: {:#?}", command, args);
+                }
+                None => {
+                    println!("No command");
+                }
+            };
+        }
+    }
+}
